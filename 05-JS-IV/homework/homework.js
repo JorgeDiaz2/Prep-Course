54f3da37291303556d5116efbd647c20ea71bac5
JorgeDiaz2 <jorgediazd777@gmail.com>
2022-03-09 23:56:34 -0300

    tarea de henryjvII

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -5,7 +5,16 @@ function crearGato(nombre, edad) {
   // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
-  // Tu código:
+  // Tu código: 
+  var objeto = {
+    nombre: nombre, 
+    edad: edad, 
+    meow: function() {
+      return "Meow!"
+    }
+    
+  } 
+  return objeto
 }
 
 
@@ -14,7 +23,11 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-}
+  
+  objeto[property] = null;
+   return objeto; 
+
+} 
 
 function invocarMetodo(objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
